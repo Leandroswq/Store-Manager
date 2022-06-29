5b48b5b403e8baf55196c6608fea7f447c263c68
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-29 18:06:31 -0300

    feat: Query que busca produtos por id

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,10 +1,18 @@
 const connection = require('./database/storeManager');
 
 module.exports = {
-  async  getAll() {
+  async getAll() {
     const query = 'SELECT * FROM StoreManager.products;';
 
     const [rows] = await connection.execute(query);
+
+    return rows;
+  },
+
+  async getByIdl(id) {
+    const query = 'SELECT * FROM StoreManager.products;';
+
+    const [rows] = await connection.execute(query, [id]);
 
     return rows;
   },
