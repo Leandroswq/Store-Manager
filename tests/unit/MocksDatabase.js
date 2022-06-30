5e74dc5ee521f2e85ae6558da3ea8206882562ea
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-30 16:06:11 -0300

    feat: criação dos mocks do banco de dados

---
+++ tests/unit/MocksDatabase.js
@@ -0,0 +1,13 @@
+module.exports = {
+  twoProducts: [
+    [
+      { id: 1, name: "Martelo de Thor" },
+      { id: 2, name: "Traje de encolhimento" },
+    ],
+    [],
+  ],
+
+  oneProducts: [[{ id: 1, name: "Martelo de Thor" }], []],
+
+  zeroProducts: [[], []],
+};
