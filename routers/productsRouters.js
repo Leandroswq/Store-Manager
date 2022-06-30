6f9c1cd2ef54cba22c754403d8eff40d72c8fff2
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-30 17:12:26 -0300

    feat: adiciona a rota '/products' do tipo post

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -4,6 +4,7 @@ const controllers = require('../controllers/productsControllers');
 const router = express.Router();
 
 router.get('/', controllers.getAll);
+router.post('/', controllers.createProduct);
 router.get('/:id', controllers.getById);
 
 module.exports = router;
