3d01ee34214c9876397a7178e969b5486ba9a18e
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-30 13:08:48 -0300

    feat: adiciona a rota /products/:id

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers/productsControllers');
 const router = express.Router();
 
 router.get('/', controllers.getAll);
+router.get('/:id', controllers.getById);
 
 module.exports = router;
