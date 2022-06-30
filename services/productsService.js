1a7d650212c929e088a4551f0e1c3da6bf6ab401
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-30 13:03:37 -0300

    feat: Criação do service getById
    Esse service chama o model getById e verifica e valida se o retorno é
    valido. Se for valido retorna o produto, caso contrario lança um erro

--- services/productsService.js
+++ services/productsService.js
@@ -8,4 +8,13 @@ module.exports = {
     if (products.length === 0) throw new error.NotFoundError('Product not found');
     return products;
   },
+
+  async getById(id) {
+    const product = await model.getById(id);
+    console.log(product);
+    if (product.length === 0) {
+      throw new error.NotFoundError('Product not found');
+    }
+      return product[0];
+  },
 };
