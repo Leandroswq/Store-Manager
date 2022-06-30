f03d007fb369ef50cf0f0ad4c1e5ec8f981c8d74
Leandroswq <leandrobonfim15@hotmail.com>
2022-06-29 22:57:45 -0300

    feat: errorMiddleware mensagens em formato de obj
    Os erros agora enviam uma mensa em formato de objetos ao invés do formato
    de string

--- middlewares/errorMiddlewarer.js
+++ middlewares/errorMiddlewarer.js
@@ -1,7 +1,8 @@
 const errorMiddleware = (err, _req, res, _next) => {
   const { name, message } = err;
+
   switch (name) {
-    case 'NotFound': return res.status(404).json(message);
+    case 'NotFound': return res.status(404).json({ message });
     default: return res.status(500).send();
   }
 };
