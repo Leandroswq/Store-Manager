41e803aba461f0b619b9be110b1006350cec276c
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-01 10:19:06 -0300

    feat: Valida o name da requisição post /products

--- services/productsService.js
+++ services/productsService.js
@@ -1,11 +1,25 @@
+const joi = require('joi');
 const model = require('../models/productsModel');
 const error = require('../errors/index');
 
 module.exports = {
+  async nameValidation(name) {
+    const required = joi.string().required().validate(name);
+    if (required.error) throw new error.BadRequestError('"name" is required');
+
+    const minLength = joi.string().required().min(5).validate(name);
+    if (minLength.error) {
+      throw new error.UnprocessableEntity(
+        '"name" length must be at least 5 characters long',
+      );
+    }
+    return true;
+  },
+
   async getAll() {
     const products = await model.getAll();
 
-    if (products.length === 0) throw new error.NotFoundError('Product not found');
+    if (products.length === 0) { throw new error.NotFoundError('Product not found'); }
     return products;
   },
 
@@ -15,7 +29,7 @@ module.exports = {
     if (product.length === 0) {
       throw new error.NotFoundError('Product not found');
     }
-      return product[0];
+    return product[0];
   },
 
   async createProduct(name) {
