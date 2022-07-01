9963d7202ac1a00264df73dca105e07230ac1c6c
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-01 10:22:49 -0300

    feat: adiciona o middleware de validação
    Adiciona o middleware de que valida o campo name da requisição post na
    rota /products

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const controllers = require('../controllers/productsControllers');
+const middlewares = require('../middlewares/productMiddlewares');
 
 const router = express.Router();
 
 router.get('/', controllers.getAll);
-router.post('/', controllers.createProduct);
+router.post('/', middlewares.nameValidation, controllers.createProduct);
 router.get('/:id', controllers.getById);
 
 module.exports = router;
