2f79b44f44f85fa0615196fcfb3cd2ff29e3fa88
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-01 11:32:54 -0300

    feat: adiciona as rotas sales/products

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const errorMiddleware = require('./middlewares/errorMiddlewarer');
 require('dotenv').config();
 
 const productsRouter = require('./routers/productsRouters');
+const salesRouter = require('./routers/salesService');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
