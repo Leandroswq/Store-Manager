b6c03110a84067e66813ef899c68b253deeb24cb
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-01 11:21:36 -0300

    test: Teste do products controller createProduct

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -12,7 +12,7 @@ describe("Testes dos products controllers", async () => {
 
   beforeEach(() => {
     request.body = {};
-    request.params = {}
+    request.params = {};
 
     response.status = sinon.stub().returns(response);
     response.json = sinon.stub().returns();
@@ -34,14 +34,28 @@ describe("Testes dos products controllers", async () => {
     it("Testa se o controller retorna o status 200 e os produtos no body do response", async () => {
       request.params.id = 1;
       const stub = sinon.stub(productsService, "getById").resolves(resolveMock);
-      await productsControllers.getById(request, response)
+      await productsControllers.getById(request, response);
 
       expect(response.status.calledWith(200)).to.be.true;
       expect(response.json.calledWith(resolveMock)).to.be.true;
       expect(stub.calledWith(request.params.id)).to.be.true;
+    });
+  });
 
+  describe("Product controller createProduct", async () => {
+    it("Testa se o controller retorna o status 201 e os produtos no body do response", async () => {
+      const stub = sinon
+        .stub(productsService, "createProduct")
+        .resolves(resolveMock);
+      
+      const name = 'blusa';
+      request.body = { name }
+      await productsControllers.createProduct(request, response)
 
-    })
-  })
+      expect(response.status.calledWith(201)).to.be.true;
+      expect(stub.calledWith(name)).to.be.true;
+      expect(response.json.calledWith(resolveMock)).to.be.true;
 
+    });
+  });
 });
