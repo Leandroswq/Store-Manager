cf2b967873728ebfe5925bfb352e6dd2615d4bb1
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-02 12:51:46 -0300

    fix: Corrige o retorno do sales service createSale
    O service retornava um objeto com as chaves "id" e "itemSold", mas o
    correto é "id" e "ItemsSold"

--- services/salesService.js
+++ services/salesService.js
@@ -4,7 +4,7 @@ module.exports = {
   async createSale(products) {
     const id = await model.createSaleId();
     await model.createSaleProducts(id, products);
-    const sale = { id, itemSold: products };
+    const sale = { id, itemsSold: products };
     return sale;
   },
 };
