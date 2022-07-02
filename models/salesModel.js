d4ca916e117892d9f4a8d7c8f9f912629f80588f
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-01 22:22:11 -0300

    feat: Criação das querys para criar uma venda
    Cria as querys createSaleId e createSaleProducts

---
+++ models/salesModel.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-lines-per-function */
+const connection = require('./database/storeManager');
+
+module.exports = {
+  async createSaleId() {
+    const querySale = 'INSERT INTO StoreManager.sales() VALUES();';
+    const [{ insertId: saleId }] = await connection.execute(querySale);
+
+    return saleId;
+  },
+  async createSaleProducts(id, products) {
+    // query inspirada no video https://youtu.be/LgLxg2WVrTY?t=724
+    const productsQuery = `INSERT INTO StoreManager.sales_products 
+    (sale_id, product_Id, quantity) VALUES ? ;`;
+    const queryValues = products.map((product) => [
+      id,
+      product.productId,
+      product.quantity,
+    ]);
+    const [rows] = await connection.query(productsQuery, [queryValues]);
+
+    return rows.affectedRows;
+  },
+};
