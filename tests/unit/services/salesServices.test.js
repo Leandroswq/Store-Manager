53b3015fc7b9b6fac8bb49849dbe90861b937744
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-03 14:27:24 -0300

    test: Test do service que retorna todas as vendas

--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -8,6 +8,16 @@ const salesService = require("../../../services/salesService");
 describe("Testes do sales services", async () => {
   afterEach(sinon.restore);
 
+  describe("ales service getAll", async () => {
+    it("Retorna todos as vendas", async () => {
+      sinon.stub(salesModel, 'getAll').resolves(1)
+
+      const response = await salesService.getAll()
+      
+      expect(response).to.equal(1)
+    })
+  })
+
   describe("Sales service createSale", async () => {
     it("Cria uma venda e retorna um objeto com o id e os produtos da venda", async() => {
           sinon.stub(salesModel, "createSaleId").resolves(1);
