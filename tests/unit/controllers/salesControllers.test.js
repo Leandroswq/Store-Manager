7751ffe5d09c52376c2eb04c28c80f711409879a
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-03 17:15:03 -0300

    test: Testa o controler getById
    Testa se o controler retorna a venda com o id do parametro id

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -29,6 +29,20 @@ describe("Testes do sales controller", async () => {
     });
   });
 
+  describe("Sales controler getById", async () => {
+    it('Retorna o status 200 com a venda com do id do parametro', async () => {
+      const stub = sinon.stub(salesService, "getById").resolves(1);
+      const id = 1;
+      request.params = {id}
+
+      await salesController.getById(request, response)
+
+      expect(stub.calledWith(id))
+      expect(response.status.calledWith(200)).to.be.true;
+      expect(response.json.calledWith(1)).to.be.true;
+    })
+  })
+
   describe("Sales controllers createSale", async () => {
     it("Retorna o status 201 e um objeto com o id da venda e os produtos criados", async () => {
       sinon
