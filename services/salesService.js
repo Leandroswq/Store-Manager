770f56102ae6bfda3fa36a759ff1399f9ea07349
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-03 13:08:32 -0300

    feat: Cria o service que retorna todas as vendas

--- services/salesService.js
+++ services/salesService.js
@@ -1,6 +1,12 @@
 const model = require('../models/salesModel');
 
 module.exports = {
+  async getAll() {
+    const sales = await model.getAll();
+
+    return sales;
+  },
+
   async createSale(products) {
     const id = await model.createSaleId();
     await model.createSaleProducts(id, products);
