5acfa2bbdec933c253b73c23e013115d3e05b4fe
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-03 13:31:41 -0300

    feat: Cria a rota para do envio da venda
    Essa rota tem como objetivo retornar para o client a venda que o client
     passar como parametro

--- routers/salesRouters.js
+++ routers/salesRouters.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', controllers.getAll);
 router.post('/', controllers.createSale);
+router.get('/:id', controllers.getById);
 
 module.exports = router;
