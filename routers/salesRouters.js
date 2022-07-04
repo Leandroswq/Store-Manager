4bf05c16aade39ef884704f19ac471126c0bf071
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-04 16:20:13 -0300

    feat: Rota para atualiza produtos de uma venda

--- routers/salesRouters.js
+++ routers/salesRouters.js
@@ -7,5 +7,6 @@ router.get('/', controllers.getAll);
 router.post('/', controllers.createSale);
 router.get('/:id', controllers.getById);
 router.delete('/:id', controllers.deleteProduct);
+router.put('/:id', controllers.updateSaleProducts);
 
 module.exports = router;
