80a360e1d79a6bf3e3f29ca9ce4dca93263902fe
Leandroswq <leandrobonfim15@hotmail.com>
2022-07-05 09:23:29 -0300

    feat: Rota para pesquisar os produtos pelo name

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 router.get('/', controllers.getAll);
 router.post('/', controllers.createProduct);
+
+router.get('/search', controllers.searchProductsByName);
+
 router.get('/:id', controllers.getById);
 router.put('/:id', controllers.updateProduct);
 router.delete('/:id', controllers.deleteProduct);
